3be85002b63dc2d7444401beab0cac9b3edaeabe
amczuboka <94947740+amczuboka@users.noreply.github.com>
2024-06-26 18:25:58 -0400

    Add Error Views
    Add ErrorView depending on status.
    Add error message if location is not available which disables button.
    Modify ContentView's layout to be more modern.

--- GoodGasoline/ContentView.swift
+++ GoodGasoline/ContentView.swift
@@ -7,63 +7,130 @@
 
 import SwiftUI
 import GoogleMaps
+import GoogleMapsUtils
 
 struct ContentView: View {
     @EnvironmentObject private var locationManager: LocationManager
+    
     var body: some View {
         NavigationView {
-            VStack {
-                Text("Welcome to Good Gasoline")
-                    .bold()
-                    .padding([.bottom], 10)
+            ZStack() {
                 Image("Road")
                     .resizable()
-                    .scaledToFit()
-                    .padding([.bottom], 300)
+                    .scaledToFill()
+                    .edgesIgnoringSafeArea(.all)
+                    .opacity(0.9)
                 
-                NavigationLink(destination: MapView(coordinate: locationManager.latestLocation?.coordinate)) {
-                    Text("View Map")
+                VStack {
+                    Text("Welcome to Good Gasoline")
+                        .bold()
+                        .font(.title)
                         .foregroundColor(.white)
-                        .padding()
-                        .background(Color.blue)
-                        .cornerRadius(10)
+                        .padding([.top], 50)
+                    
+                    Spacer()
+                    
+                    if !locationManager.isLocationAvailable {
+                        Text("Location is not available. May be due to poor GPS signal.")
+                            .frame(width: UIScreen.main.bounds.width * 0.8)
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(.red)
+                            .padding(.bottom, 20)
+                    }
+                    
+                    NavigationLink(destination: MapView()) {
+                        Text("View Map")
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                    }
+                    .hoverEffect(.lift)
+                    .buttonStyle(PlainButtonStyle())
+                    .padding()
+                    .disabled(!locationManager.isLocationAvailable)
                 }
-                .buttonStyle(PlainButtonStyle())
                 .padding()
+                
+                if let errorMessage = locationManager.locationErrorMessage {
+                    ErrorView(errorMessage: errorMessage)
+                }
             }
-            .padding()
         }
     }
 }
+
+
 struct MapView: UIViewRepresentable {
     @EnvironmentObject private var locationManager: LocationManager
-    private var coordinate: CLLocationCoordinate2D?
+    @State private var zoomLevel: Float = 16.0
+
+    // Handle map events
+    func makeCoordinator() -> Coordinator {
+        Coordinator(parent: self)
+    }
     
-    init(coordinate: CLLocationCoordinate2D?) {
-        self.coordinate = coordinate
+    class Coordinator: NSObject, GMSMapViewDelegate {
+        var parent: MapView
+        
+        init(parent: MapView) {
+            self.parent = parent
+        }
+
+        func mapView(_ mapView: GMSMapView, didChange position: GMSCameraPosition) {
+            print("zoom changed on didChange")
+            parent.zoomLevel = position.zoom
+        }
+        
+        func mapView(_ mapView: GMSMapView, idleAt position: GMSCameraPosition) {
+            print("zoom changed on idleAt")
+            parent.zoomLevel = position.zoom
+        }
     }
     
     func makeUIView(context: Context) -> GMSMapView {
-        let camera = GMSCameraPosition.camera(withLatitude: coordinate?.latitude ?? 0, longitude: coordinate?.longitude ?? 0, zoom: 6.0)
+        let camera = GMSCameraPosition.camera(withLatitude: locationManager.latestLocation?.coordinate.latitude ?? 0, longitude: locationManager.latestLocation?.coordinate.longitude ?? 0, zoom: zoomLevel)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        print("License: \n\n\(GMSServices.openSourceLicenseInfo())")
+        
+        mapView.delegate = context.coordinator
+        
+        // Enable gestures
+        mapView.settings.scrollGestures = true
+        mapView.settings.zoomGestures = true
+        mapView.settings.tiltGestures = true
+        mapView.settings.rotateGestures = true
+        
         return mapView
     }
     
     func updateUIView(_ mapView: GMSMapView, context: Context) {
         let marker = GMSMarker()
         
-        if let userCoordinate = coordinate {
-            marker.position = userCoordinate
-            marker.title = "Current Location"
-            
+        if let userCoordinate = locationManager.latestLocation?.coordinate {
+                    marker.position = userCoordinate
+                    marker.title = "Current Location"
         }
+        
         marker.map = mapView
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct ErrorView: View {
+    var errorMessage: String?
+
+    var body: some View {
+        VStack {
+            if let errorMessage = errorMessage {
+                Text(errorMessage)
+                    .frame(width: UIScreen.main.bounds.width * 0.8)
+                    .foregroundColor(.red)
+                    .multilineTextAlignment(.center)
+            } else {
+                Text("Unknown error occurred.")
+                    .foregroundColor(.red)
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.white)
     }
 }
--- GoodGasoline/LocationManager.swift
+++ GoodGasoline/LocationManager.swift
@@ -10,25 +10,48 @@ import CoreLocation
 class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
     private let locationManager = CLLocationManager()
     @Published var latestLocation: CLLocation?
+    @Published var locationErrorMessage: String?
     
+    var simulatedAuthorizationStatus: CLAuthorizationStatus?
+
     override init() {
         super.init()
         
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
-        locationManager.startUpdatingLocation()
     }
+    
+    var isLocationAvailable: Bool {
+        return latestLocation != nil
+    }
+    
+    var isLocationServicesEnabled: Bool {
+        return CLLocationManager.locationServicesEnabled() &&
+            (locationManager.authorizationStatus == .authorizedWhenInUse ||
+            locationManager.authorizationStatus == .authorizedAlways)
+    }
+
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.first {
             latestLocation = location
+            print("Updated location: \(location.coordinate.latitude), \(location.coordinate.longitude)")
         }
     }
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        if status == .authorizedWhenInUse {
-            locationManager.requestLocation()
+        let statusToCheck = simulatedAuthorizationStatus ?? status
+        
+        switch statusToCheck {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted, .denied:
+            locationErrorMessage = "Location permissions are denied or restricted. Please enable them in Settings."
+        case .authorizedWhenInUse, .authorizedAlways:
+            locationManager.startUpdatingLocation()
+        @unknown default:
+            locationErrorMessage = "An unknown error occurred. Please try again."
         }
     }
     
